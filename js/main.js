4aa0ec2f59a25e42ea44c03d97c2cc16d6d8bfd4
vishalicious213 <vishalicious213@gmail.com>
2024-12-23 22:04:54 -0800

    changed h2 innertext in renderGame() and decreased font-size

--- js/main.js
+++ js/main.js
@@ -112,11 +112,11 @@ function renderGame(player1Val, player2Val) {
   console.log(isWar)
   if (player1Val > player2Val) {
     isWar ? player1Score += 8 : player1Score +=2
-    document.querySelector("h2").innerText = `Player 1 Wins`
+    document.querySelector("h2").innerText = `Player 1 Wins Round`
     document.querySelector("#player1-cards").innerText = player1Score
   } else if (player1Val < player2Val) {
     isWar ? player2Score += 8 : player2Score +=2
-    document.querySelector("h2").innerText = `Player 2 Wins`
+    document.querySelector("h2").innerText = `Player 2 Wins Round`
     document.querySelector("#player2-cards").innerText = player2Score
   } else {
     document.querySelector("h2").innerText = `Time for WAR!`
